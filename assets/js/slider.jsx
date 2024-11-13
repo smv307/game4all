2067d46632e0ec6afcf60cc5445312ab81e14f8d
sloane <103333297+smv307@users.noreply.github.com>
2024-11-12 18:32:14 -0800

    Add files via upload
    Static Header and filtering system

---
+++ assets/js/slider.jsx
@@ -0,0 +1,14 @@
+// update player count filter label
+const playerFilter = document.getElementById('player-filter');
+const playerFilterLabel = document.getElementById('player-filter-label');
+
+    function updatePlayerFilterLabel() {
+        if (playerFilter.value > 0) { // 0 = no filter
+            playerFilterLabel.textContent = playerFilter.value;
+        } else {
+            playerFilterLabel.textContent = '#';
+        }
+    };
+
+    updatePlayerFilterLabel(); // show label when the page loads
+    playerFilter.addEventListener('input', updatePlayerFilterLabel);
